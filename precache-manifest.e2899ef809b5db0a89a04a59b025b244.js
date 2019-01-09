8dde0e0f99b81bdcabe4d538833c3179f637aec2
Tero Kettunen <terojkettunen@gmail.com>
2019-01-09 09:24:28 +0200

    Updates

---
+++ precache-manifest.e2899ef809b5db0a89a04a59b025b244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9613cde595a03f806188",
+    "url": "/exercise-demo-react/static/css/main.1978dd2a.chunk.css"
+  },
+  {
+    "revision": "9613cde595a03f806188",
+    "url": "/exercise-demo-react/static/js/main.9613cde5.chunk.js"
+  },
+  {
+    "revision": "2539cc5a94163bfbc182",
+    "url": "/exercise-demo-react/static/js/1.2539cc5a.chunk.js"
+  },
+  {
+    "revision": "1286e19b632ae823ec4a",
+    "url": "/exercise-demo-react/static/js/runtime~main.1286e19b.js"
+  },
+  {
+    "revision": "9fa9d2c231f6dd1527df640515f06570",
+    "url": "/exercise-demo-react/index.html"
+  }
+];
